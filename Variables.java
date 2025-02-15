1478e585fb83d1e969287bf99e999500b79fde8b
tumonbaev1 <e.tumonbai@gmail.com>
2025-02-14 21:53:55 -0800

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args){
+        int age = 23;
+        double height = 72;
+        char grade = 'A';
+        boolean isActive = false;
+        String name = "Elturan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + "t");
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Active?: " + isActive);
+
+    }
+}
