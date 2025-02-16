6f87ba07fbc0169b03abb1b78971fa0e554f94e1
tumonbaev1 <e.tumonbai@gmail.com>
2025-02-15 21:52:11 -0800

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args){
+        int number = -5;
+        if (number > 0) {
+            System.out.println("Positive number ");
+        } else {
+            System.out.println("Negative number or zero ");
+
+        }
+    }
+}
