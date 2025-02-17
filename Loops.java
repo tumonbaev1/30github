3b0460598f81b35aa9b9190cc88143b727c71987
tumonbaev1 <e.tumonbai@gmail.com>
2025-02-16 21:24:58 -0800

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    public static void main(String[] args) {
+        // for loop: print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
