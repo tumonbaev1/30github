35767f287055db4795939370ecd517380b544c85
tumonbaev1 <e.tumonbai@gmail.com>
2025-02-18 22:06:57 -0800

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+        public static void main(String[] args) {
+            greet("Elturan"); 
+            greet("Daniel");
+        }
+
+        public static void greet(String name) {
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+
