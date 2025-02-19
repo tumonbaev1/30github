9780272134cc5ed348e8d0da9a95659aa17365bb
tumonbaev1 <e.tumonbai@gmail.com>
2025-02-19 12:50:03 -0800

    Add files via upload

---
+++ Debugging.java
@@ -0,0 +1,10 @@
+public class Debugging {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b; //  Поменял - на +
+
+        System.out.println("Sum: " + sum);
+    }
+
+}
